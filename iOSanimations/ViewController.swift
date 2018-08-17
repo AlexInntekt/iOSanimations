f0b1bf7b09b2591deddc00ef3113d89c0fb5397b
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-18 01:19:30 +0300

    Displacement Animations without Autolayout

--- iOSanimations/ViewController.swift
+++ iOSanimations/ViewController.swift
@@ -7,18 +7,31 @@
 //
 
 import UIKit
+import Foundation
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet var textFieldOne: UITextField!
+    @IBOutlet var textFieldTwoo: UITextField!
+    @IBOutlet var button: UIButtonX!
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        self.textFieldOne.center.x -= self.view.bounds.width
+        self.textFieldTwoo.center.x -= self.view.bounds.width
+        self.button.center.x -= self.view.bounds.width
+        
+        //textFieldOne.center.x  -= view.bounds.width
+        UIView.animate(withDuration: 0.7) {
+            self.textFieldOne.center.x += self.view.bounds.width
+            self.textFieldTwoo.center.x += self.view.bounds.width
+            self.button.center.x += self.view.bounds.width
+        }
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
 
 
 }
