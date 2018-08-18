14bbc00dba285ac66f119e68b4bfc1afa27035c1
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-18 09:09:10 +0300

    Button dissappearing after pressing it

--- iOSanimations/ViewController.swift
+++ iOSanimations/ViewController.swift
@@ -25,8 +25,21 @@ class ViewController: UIViewController
         
     }
     
+    @IBAction func button(_ sender: Any)
+    {
+        //do stuff
+        
+        UIView.animate(withDuration: 0.5, delay: 0.0, usingSpringWithDamping: 0.0, initialSpringVelocity: 0.0, options: [], animations:
+        {
+            self.button.center.y += 70
+            self.button.alpha = 0
+        })
+    }
+    
     func initialUISetup()
     {
+        self.textFieldTwoo.isSecureTextEntry = true //password field
+        
         self.textFieldOne.center.x -= self.view.bounds.width
         self.textFieldTwoo.center.x -= self.view.bounds.width
         self.button.center.x -= self.view.bounds.width
@@ -34,18 +47,18 @@ class ViewController: UIViewController
     
     func chainedAnimations()
     {
-        UIView.animate(withDuration: 0.9, delay: 0.4, options: [.curveEaseOut], animations:
+        UIView.animate(withDuration: 0.6, delay: 0.4, options: [.curveEaseOut], animations:
           {
             self.textFieldOne.center.x += self.view.bounds.width
           },
             completion:
              { _ in
-                UIView.animate(withDuration: 0.9, delay: 0.3, options: [.curveEaseOut], animations:
+                UIView.animate(withDuration: 0.6, delay: 0.3, options: [.curveEaseOut], animations:
                   {
                      self.textFieldTwoo.center.x += self.view.bounds.width
                   }, completion:
                      { _ in
-                        UIView.animate(withDuration: 0.9, delay: 0.1, options: [.curveEaseOut], animations:
+                        UIView.animate(withDuration: 0.8, delay: 0.1, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.0 , options: [.curveEaseOut], animations:
                         {
                                 self.button.center.x += self.view.bounds.width
                         }, completion: nil)
