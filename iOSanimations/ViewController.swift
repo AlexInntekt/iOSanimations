1f311c1fdef40b3a7435f1288c46b3b5d4b4fb40
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-23 20:41:02 +0300

    -animated the button’s rotation -check page 107 if you want to see how to effect a whole set of constraints that belong to one view -some more customized configurations for the layout changes.

--- iOSanimations/ViewController.swift
+++ iOSanimations/ViewController.swift
@@ -24,20 +24,62 @@ class ViewController: UIViewController
     }
 
 
+    @IBOutlet var butonMenu: UIButton!
     @IBAction func actionToggleMenu(_ sender: Any)
-    {
+    {   //check if menu is expanded or shrinked
         isMenuOpen = !isMenuOpen
-        menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+        
+        
+        
+        //order changes in the layout, the changes will be animated in the first part of the animation
+        titleLabel.superview?.constraints.forEach   //change x coordinate of the title
+        { constraint in
+            if constraint.firstItem === titleLabel &&
+                constraint.firstAttribute == .centerX
+             {
+                
+                constraint.constant = isMenuOpen ? -100.0 : 0.0
+                return
+             }
+            //change y of title
+            if constraint.identifier == "TitleCenterY"
+             {
+                constraint.isActive = false
+                print("924hg294hg")
+                let newConstraint = NSLayoutConstraint(
+                    item: titleLabel,
+                    attribute: .centerY,
+                    relatedBy: .equal,
+                    toItem: titleLabel.superview!,
+                    attribute: .centerY,
+                    multiplier: isMenuOpen ? 0.67 : 1.0,
+                    constant: 5.0)
+                newConstraint.identifier = "TitleCenterY"
+                newConstraint.isActive = true
+             }
+                
+        }
+        
+        //order changes in the layout, the changes will be animated in the first part of the animation
+        menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0  //height of menu
         titleLabel.text = isMenuOpen ? "Select Item" : "Packing List"
+        
         
         UIView.animate(withDuration: 1.0, delay: 0.0,
                        usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0,
                        options: .curveEaseIn,
                        animations: {
-                        self.view.layoutIfNeeded()
+                        self.view.layoutIfNeeded()  //animate the change of height of the menu
+                        
+                        //animate the rotation of the button
+                        let angle: CGFloat = self.isMenuOpen ? .pi / 4 : 0.0
+                        self.butonMenu.transform = CGAffineTransform(rotationAngle: angle)
+                        
         },
                        completion: nil
         )
+        
+        
     }
     
 
