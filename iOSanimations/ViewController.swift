83647d452da37d48281d238a02edc2ddd07cc7ea
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-08-23 20:03:12 +0300

    Started to work on the animation of the expanding menu. Beautiful so far!

--- iOSanimations/ViewController.swift
+++ iOSanimations/ViewController.swift
@@ -8,17 +8,38 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    override func viewDidLoad() {
+class ViewController: UIViewController
+{
+    @IBOutlet weak var menuHeightConstraint: NSLayoutConstraint!
+    @IBOutlet var titleLabel: UILabel!
+    
+    var isMenuOpen = false
+    
+    override func viewDidLoad()
+    {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+      
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+
+    @IBAction func actionToggleMenu(_ sender: Any)
+    {
+        isMenuOpen = !isMenuOpen
+        menuHeightConstraint.constant = isMenuOpen ? 200.0 : 60.0
+        titleLabel.text = isMenuOpen ? "Select Item" : "Packing List"
+        
+        UIView.animate(withDuration: 1.0, delay: 0.0,
+                       usingSpringWithDamping: 0.4, initialSpringVelocity: 10.0,
+                       options: .curveEaseIn,
+                       animations: {
+                        self.view.layoutIfNeeded()
+        },
+                       completion: nil
+        )
     }
+    
 
 
 }
