c1f029a014a8171d0f9ec9f8d4b6398658a27a22
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-09-03 23:29:56 +0300

    Animating a gradient

--- iOSanimations/AnimatedMaskLabel.swift
+++ iOSanimations/AnimatedMaskLabel.swift
@@ -24,28 +24,68 @@ import UIKit
 import QuartzCore
 
 @IBDesignable
-class AnimatedMaskLabel: UIView {
+class AnimatedMaskLabel: UIView
+{
     
-    let gradientLayer: CAGradientLayer = {
+    let gradientLayer: CAGradientLayer =
+    {
         let gradientLayer = CAGradientLayer()
         
         // Configure the gradient here
+        gradientLayer.startPoint = CGPoint(x: 0.0, y: 0.5)
+        gradientLayer.endPoint = CGPoint(x: 1.0, y: 0.5)
+        
+        let colors = [
+            UIColor.black.cgColor,
+            UIColor.white.cgColor,
+            UIColor.black.cgColor
+        ]
+        gradientLayer.colors = colors
+        
+        let locations: [NSNumber] = [
+            0.25,
+            0.5,
+            0.75
+        ]
+        gradientLayer.locations = locations
+        
+        
         
         return gradientLayer
     }()
     
-    @IBInspectable var text: String! {
+    
+    @IBInspectable var text: String!
+        {
         didSet {
             setNeedsDisplay()
         }
     }
     
-    override func layoutSubviews() {
+    override func layoutSubviews()
+    {
         layer.borderColor = UIColor.green.cgColor
+        
+        gradientLayer.frame = CGRect(
+            x: -bounds.size.width,
+            y: bounds.origin.y,
+            width: 3 * bounds.size.width,
+            height: bounds.size.height)
     }
     
-    override func didMoveToWindow() {
+    override func didMoveToWindow()
+    {
         super.didMoveToWindow()
+        
+        layer.addSublayer(gradientLayer)
+        
+        let gradientAnimation = CABasicAnimation(keyPath: "locations")
+        gradientAnimation.fromValue = [0.0, 0.0, 0.25]
+        gradientAnimation.toValue = [0.75, 1.0, 1.0]
+        gradientAnimation.duration = 2.5
+        gradientAnimation.repeatCount = Float.infinity
+        
+        gradientLayer.add(gradientAnimation, forKey: nil)
         
     }
     
