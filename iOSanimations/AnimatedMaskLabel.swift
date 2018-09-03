fa0ad1e9cf0fed7b792731c6ef2f57263b51cc73
Manolescu Mihai Alexandru <alex.inntekt@gmail.com>
2018-09-03 21:46:15 +0300

    Initializing branch with starter folder

--- iOSanimations/AnimatedMaskLabel.swift
+++ iOSanimations/AnimatedMaskLabel.swift
@@ -21,44 +21,33 @@
  */
 
 import UIKit
+import QuartzCore
 
-class ViewController: UIViewController {
+@IBDesignable
+class AnimatedMaskLabel: UIView {
     
-    @IBOutlet var slideView: AnimatedMaskLabel!
-    @IBOutlet var time: UILabel!
-    
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    let gradientLayer: CAGradientLayer = {
+        let gradientLayer = CAGradientLayer()
         
+        // Configure the gradient here
+        
+        return gradientLayer
+    }()
+    
+    @IBInspectable var text: String! {
+        didSet {
+            setNeedsDisplay()
+        }
     }
     
-    func didSlide() {
+    override func layoutSubviews() {
+        layer.borderColor = UIColor.green.cgColor
+    }
+    
+    override func didMoveToWindow() {
+        super.didMoveToWindow()
         
-        // reveal the meme upon successful slide
-        let image = UIImageView(image: UIImage(named: "meme"))
-        image.center = view.center
-        image.center.x += view.bounds.size.width
-        view.addSubview(image)
-        
-        UIView.animate(withDuration: 0.33, delay: 0.0,
-                       animations: {
-                        self.time.center.y -= 200.0
-                        self.slideView.center.y += 200.0
-                        image.center.x -= self.view.bounds.size.width
-        },
-                       completion: nil
-        )
-        
-        UIView.animate(withDuration: 0.33, delay: 1.0,
-                       animations: {
-                        self.time.center.y += 200.0
-                        self.slideView.center.y -= 200.0
-                        image.center.x += self.view.bounds.size.width
-        },
-                       completion: {_ in
-                        image.removeFromSuperview()
-        }
-        )
     }
     
 }
+
